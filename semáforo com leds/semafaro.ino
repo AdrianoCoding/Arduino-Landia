0f73eda894fb35828031bbaa717ae12f87c9267d
Adriano J <70298185+sudoshift@users.noreply.github.com>
2023-06-01 11:39:23 -0300

    Baixe o script do semaforo

--- semáforo com leds/semafaro.ino
+++ semáforo com leds/semafaro.ino
@@ -1,6 +1,6 @@
-const int ledPin1 = 8;
-const int ledPin2 = 9;
-const int ledPin3 = 10;
+const int ledPin1 = 13;
+const int ledPin2 = 12; //Define as portas do led 1, 2 e 3
+const int ledPin3 = 8;
 
 void setup() {
   pinMode(ledPin1, OUTPUT);
@@ -9,18 +9,18 @@ void setup() {
 }
 
 void loop() {
-  analogWrite(ledPin1, 200);  // Define o valor para o brilho no LED 1
+  digitalWrite(ledPin1, HIGH);  // Liga o LED 1
   delay(3000);
-  analogWrite(ledPin1, 0);    // Desliga o LED 1 (Verde)
+  digitalWrite(ledPin1, LOW);    // Desliga o LED 1 (Verde)
   delay(100);
   
-  analogWrite(ledPin2, 200);  // Define o valor para o brilho no LED 2
+  digitalWrite(ledPin2, HIGH);  // Liga o LED 2
   delay(500);
-  analogWrite(ledPin2, 0);    // Desliga o LED 2 (Amarelo)
+  digitalWrite(ledPin2, LOW);    // Desliga o LED 2 (Amarelo)
   delay(100);
   
-  analogWrite(ledPin3, 200);  // Define o valor para o brilho no LED 3
+  digitalWrite(ledPin3, HIGH);  // Liga o LED 3
   delay(3000);
-  analogWrite(ledPin3, 0);    // Desliga o LED 3 (Vermelho)
+  digitalWrite(ledPin3, LOW);    // Desliga o LED 3 (Vermelho)
   delay(100);
 }
